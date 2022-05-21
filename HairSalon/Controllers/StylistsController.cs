a71b439afde1d112fc2b6ede2da5fa69654c5cb7
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-21 01:04:49 -0700

    Create the clients list from the clients.cs file with the Clientscontroller.cs file

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController: Controller
+  {
+    
+  }
+
+
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,11 +7,11 @@ namespace HairSalon.Models
   {
     public Stylist()
     {
-      // this.Clients = new HashSet<Client>();
+      this.Clients = new HashSet<Client>();
     }
-    public int StylistsId { get; set; }
+    public int StylistId { get; set; }
     public string Name { get; set; }
     public string Specialty { get; set; }
-    // public virtual ICollection<Client> Clients { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
