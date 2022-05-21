bbb0e60f1dbf48a96fc512d6a5797f6430d672b3
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-20 23:46:55 -0700

    Create the necessary models and assign their values

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+// using HairSalon.Models;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistsId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
